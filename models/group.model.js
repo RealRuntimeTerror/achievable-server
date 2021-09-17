561b6f15c58467c9007bfe8d27302f4c82a1c2c4
Skenos Peniel <skenos3s@gmail.com>
2021-09-17 18:01:33 +0530

    Model #1

---
+++ models/group.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Activity = require("./activity.model");
+const User = require("./user.model");
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema(
+  {
+    groupname: { type: String, required: true },
+    members: [{ type: User, default: null }],
+    activities: [{ type: Activity, default: null }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Group = mongoose.model("Group", groupSchema);
+
+module.exports = Group;
