f8aaccff432ba6327b43da3dd207a33fd450d8bf
Rukshan Karannagoda <58694077+rukshankr@users.noreply.github.com>
2021-09-17 18:04:40 +0530

    intialize node express backend

---
+++ server.js
@@ -0,0 +1,25 @@
+//where we put all our server code
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('db opened'))
+
+app.use(express.json()) //lets server accept json
+
+const activitiesRouter = require('./routes/activities')
+app.use('/activities', activitiesRouter)
+
+app.listen(3000, console.log("Server started"))
