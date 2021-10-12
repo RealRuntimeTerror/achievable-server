43ed36668c30cf9234d1a03b000885ccdfd94ed7
Skenos Peniel <skenos3s@gmail.com>
2021-10-12 18:27:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 //where we put all our server code
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
 
-const activitiesRouter = require('./routes/activities')
-const groupsRouter = require('./routes/groups')
-const usersRouter = require('./routes/users')
-const sessionsRouter = require('./routes/sessions')
+const activitiesRouter = require("./routes/activities");
+const groupsRouter = require("./routes/groups");
+const usersRouter = require("./routes/users");
+const sessionsRouter = require("./routes/sessions");
 
-app.use(cors({
-  origin: '*'
-}));
-
-mongoose.connect(process.env.DATABASE_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+app.use(
+  cors({
+    origin: "*",
+  })
 );
+
+app.set("port", process.env.PORT || 3000);
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('error', (error) => console.error(error))
-db.once('open', async() => await console.log('db opened'))
+db.on("error", (error) => console.error(error));
+db.once("open", async () => await console.log("db opened"));
 
+app.use(express.json()); //lets server accept json
 
-app.use(express.json()) //lets server accept json
+app.use("/activities", activitiesRouter);
+app.use("/groups", groupsRouter);
+app.use("/users", usersRouter);
+app.use("/sessions", sessionsRouter);
 
-
-app.use('/activities', activitiesRouter)
-app.use('/groups', groupsRouter)
-app.use('/users', usersRouter)
-app.use('/sessions', sessionsRouter)
-
-module.exports = app
+module.exports = app;
