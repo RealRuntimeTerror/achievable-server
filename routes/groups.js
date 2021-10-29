949089986366108287ea9a405ce710a2d4b279f0
Rukshan Karannagoda <58694077+rukshankr@users.noreply.github.com>
2021-10-29 15:49:33 +0530

    add query and leave group

--- routes/groups.js
+++ routes/groups.js
@@ -5,91 +5,139 @@ const getById = require('../middleware/get-by-id')
 const User = require('../models/user.model')
 
 //getting all groups
-router.get('/', async (req,res) => {
-    try{
+router.get('/', async (req, res) => {
+    try {
         const groups = await Group.find()
         res.json(groups)
     }
-    catch (err){
-        res.status(500).json({message: err.message})
+    catch (err) {
+        res.status(500).json({ message: err.message })
     }
 })
 //getting all groups + admin info
-router.get('/admin', async (req,res) => {
-    try{
+router.get('/admin', async (req, res) => {
+    try {
         const groups = await Group.aggregate([
             {
-              $lookup:
+                $lookup:
                 {
-                  from: User.collection.name,
-                  localField: "adminId",
-                  foreignField: "googleId",
-                  as: "adminData"
+                    from: User.collection.name,
+                    localField: "adminId",
+                    foreignField: "googleId",
+                    as: "adminData"
                 }
-           }
-         ])
+            }
+        ])
         res.json(groups)
     }
-    catch (err){
-        res.status(500).json({message: err.message})
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+//getting groups that match query name
+router.get('/search/:query', async (req,res) => {
+    try {
+        const groups = await Group.aggregate([
+            {$match: {groupName: {$regex: '.*' + req.params.query + '.*', $options: 'ix'}}},
+            {
+                $lookup:
+                {
+                    from: User.collection.name,
+                    localField: "adminId",
+                    foreignField: "googleId",
+                    as: "adminData"
+                }
+            }
+        ]).limit(5)
+        res.json(groups)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
     }
 })
 
 //getting only groups that the user is in 
-router.get('/user/:uid',getById({type: "Usergroup"}), (req,res) => {
+router.get('/user/:uid', getById({ type: "Usergroup" }), (req, res) => {
     res.send(res.group)
 })
 
 //getiing a group
-router.get('/:id',getById({type: "group"}), (req,res) => {
+router.get('/:id', getById({ type: "group" }), (req, res) => {
     res.send(res.group)
 })
 
 //creating group
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     const group = new Group({
         groupName: req.body.groupName,
         adminId: req.body.adminId,
-        activities: req.body.activities
+        activities: req.body.activities,
+        members: req.body.members
     })
-    try{
+    try {
         const newGroup = await group.save();
         res.status(201).json(newGroup) //201 - creation successful
     }
-    catch(err){
-        res.status(400).json({message: err.message})//400 client side error - bad input
+    catch (err) {
+        res.status(400).json({ message: err.message })//400 client side error - bad input
     }
 })
 
 //updating group --> patch cause we only want to update one field
-router.patch('/:id', getById({type: "group"}), async (req,res) => {
-    if (req.body.groupname != null){
+router.patch('/:id', getById({ type: "group" }), async (req, res) => {
+    if (req.body.groupname != null) {
         res.group.groupName = req.body.groupName
     }
-    if (req.body.adminId != null){
+    if (req.body.adminId != null) {
         res.group.adminId = req.body.adminId
     }
-    if (req.body.activities != null){
+    if (req.body.activities != null) {
         res.group.activities.push(req.body.activities)
     }
-    try{
+    try {
         const updatedGroup = await res.group.save()
         res.json(updatedGroup)
     }
-    catch (err){
-        res.status(400).json({message: err.message})
+    catch (err) {
+        res.status(400).json({ message: err.message })
     }
 })
 
 //deletion 
-router.delete('/:id', getById({type: "group"}), async (req,res) => {
-    try{
+router.delete('/:id', getById({ type: "group" }), async (req, res) => {
+    try {
         res.group.remove()
-        res.json({message: 'Deleted Group'})
+        res.json({ message: 'Deleted Group' })
     }
-    catch(err){
-        res.status(500).json({message: err.message})
+    catch (err) {
+        res.status(500).json({ message: err.message })
     }
+})
+
+//join group
+router.patch('/join/:id', getById({type: "group"}), async(req,res) =>{
+    res.group.members.push(req.body.googleId);
+    try {
+        const updatedGroup = await res.group.save()
+        res.json(updatedGroup)
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+//leave group
+router.patch('/leave/:id', getById({ type: "group" }), async (req, res) => {
+    res.group.members = res.group.members.filter((m) => m != req.body.googleId);
+    try {
+        const updatedGroup = await res.group.save()
+        res.json(updatedGroup)
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+
 })
 
 module.exports = router
